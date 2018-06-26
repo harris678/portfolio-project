0a7057fa99bc98bef421447fc86077b72f10171d
Harish Bhakuni <harishbhakuni21@gmail.com>
2018-06-26 22:04:11 +0530

    Last br comit hai

--- jobs/views.py
+++ jobs/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Job
 def home(request):
     jobs = Job.objects
+    print('hello')
     return render(request,'jobs/home.html',{'jobs':jobs})
