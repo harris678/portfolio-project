44cc5bf12fafd5a4d575ff7442a61cbc490219b7
Harish Bhakuni <harishbhakuni21@gmail.com>
2018-06-26 21:06:29 +0530

    Final Commit Last

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,5 @@ class Blog(models.Model):
     title = models.CharField(max_length = 255)
     pub_date = models.DateTimeField(auto_now=False)
     body = models.TextField()
+    summary = models.TextField(null=True,blank=True)
     image = models.ImageField(upload_to = 'images/')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Blog
+def allblogs(request):
+    blogs = Blog.objects
+    return render(request,'blog/allblogs.html',{'blogs':blogs})
 
-# Create your views here.
+def detail(request,blog_id):
+    blogs = Blog.objects.filter(id=blog_id)
+    blogs = blogs.first()
+    return render(request,'blog/detail.html',{'blogs':blogs})
