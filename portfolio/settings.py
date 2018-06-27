851c1bef6613648b21337f97f9c608ca7ccf4dc8
Harish Bhakuni <harishbhakuni21@gmail.com>
2018-06-27 15:41:29 +0530

    final commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -104,6 +104,17 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
