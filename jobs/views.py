9aec5e65675fbf597653ca7078874ff28c8d7950
Harish Bhakuni <harishbhakuni21@gmail.com>
2018-06-27 17:19:55 +0530

    changed python version

--- jobs/views.py
+++ jobs/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from .models import Job
 def home(request):
     jobs = Job.objects
-    print('hello')
     return render(request,'jobs/home.html',{'jobs':jobs})
